0797d03ecb0df6ce8443b3895f80aca3048efd36
shitikakei <shitikakei@gmail.com>
2020-07-17 04:07:32 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shitikamiyako/React_Tutorial2.git/precache-manifest.bdb273a9b6d729bbd52a2ba8bf41f202.js"
+  "/React_Tutorial2/precache-manifest.3a1699360457b21e0b95fb20920713c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shitikamiyako/React_Tutorial2.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Tutorial2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
